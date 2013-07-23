b861bc0263ffb0dfae72ac7a90f967fd9b523893
antonio <antonio.rubio@m-hunter.com>
2013-07-23 11:38:09 +0200

    triangle_exceptions

--- triangle.rb
+++ triangle.rb
@@ -13,14 +13,29 @@
 # and
 #   about_triangle_project_2.rb
 #
+def less_than_zero(a, b, c)
+	return (a < 0 or b < 0 or c < 0)
+end
+
+def wrong_sum_triangle_sides(a, b, c)
+	return ((a + b + c <= 0) and ((a + b < c) or (a + c < b) or (c + b < a)))
+end
+
+def error_triangle(a, b, c)
+	return  (less_than_zero(a, b,c) or wrong_sum_triangle_sides(a, b, c))
+end
+
 def triangle(a, b, c)
-  if a == b and b == c
-    :equilateral 
-  elsif a == b or b == c or a == c
-    :isosceles
-  else
-    :scalene    
-  end
+	if error_triangle(a, b, c)
+		raise TriangleError
+	end
+	if a == b and b == c
+    	:equilateral 
+  	elsif a == b or b == c or a == c
+    	:isosceles
+  	else
+    	:scalene    
+  	end
 end
 
 # Error class used in part 2.  No need to change this code.
